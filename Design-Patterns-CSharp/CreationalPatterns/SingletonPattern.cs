ce5ea0867657a5b5bc394d6379fe922dd760bc45
itsimtiaz <itsimtiaz@outlook.com>
2025-05-23 18:41:42 +0500

    Added Singleton Pattern Example.

---
+++ Design-Patterns-CSharp/CreationalPatterns/SingletonPattern.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace Design_Patterns_CSharp.CreationalPatterns;
+
+public class SingletonPattern
+{
+
+    private static SingletonPattern? _instance;
+    private readonly List<int> _numbers;
+    private static readonly object _instanceLock = new();
+
+    private SingletonPattern()
+    {
+        _numbers = new List<int>();
+    }
+
+    public void Add(int number) => _numbers.Add(number);
+
+    public IEnumerable<int> GetNumbers() => _numbers.AsReadOnly();
+
+    public static SingletonPattern GetInstance()
+    {
+        if (_instance is null)
+        {
+            lock (_instanceLock)
+            {
+                if (_instance is null)
+                    _instance = new();
+            }
+        }
+
+        return _instance;
+    }
+
+}
+
+public class SingletonDemo
+{
+    public static void Run()
+    {
+        var instance = SingletonPattern.GetInstance();
+
+        instance.Add(1);
+        instance.Add(2);
+        instance.Add(3);
+
+        var anotherInstance = SingletonPattern.GetInstance();
+        anotherInstance.Add(4);
+        anotherInstance.Add(5);
+
+        Console.WriteLine($"numbers are {string.Join(", ", instance.GetNumbers())}");
+    }
+}
--- Design-Patterns-CSharp/Program.cs
+++ Design-Patterns-CSharp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Design_Patterns_CSharp.CreationalPatterns;
+
 Console.WriteLine("Hello, World!");
+
+SingletonDemo.Run();
