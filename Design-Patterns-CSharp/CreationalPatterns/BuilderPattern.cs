87c83ec84eac96f3032079b11ffc508d37392a3e
itsimtiaz <itsimtiaz@outlook.com>
2025-05-29 00:36:14 +0500

    Add an example of Builder pattern.

---
+++ Design-Patterns-CSharp/CreationalPatterns/BuilderPattern.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text;
+
+namespace Design_Patterns_CSharp.CreationalPatterns;
+
+interface IQueryBuilder
+{
+    IQueryBuilder AddParam(string name, string value);
+    string Build();
+}
+
+class QueryBuilder : IQueryBuilder
+{
+    StringBuilder resultUrl = new();
+
+    public IQueryBuilder AddParam(string name, string value)
+    {
+        if (resultUrl.Length == 0)
+            resultUrl.Append("?");
+        else
+            resultUrl.Append("&");
+
+        resultUrl.Append($"{name}={value}");
+
+        return this;
+    }
+
+    public string Build() => resultUrl.ToString();
+}
+
+class QueryBuilderDirector
+{
+    private readonly IQueryBuilder queryBuilder;
+
+    public QueryBuilderDirector(IQueryBuilder queryBuilder)
+    {
+        this.queryBuilder = queryBuilder;
+    }
+
+    public string CreateProfileLink() => queryBuilder.AddParam("profileId", "1").Build();
+}
+
+public class BuilderPatternDemo
+{
+    public static void Run()
+    {
+        IQueryBuilder queryBuilder = new QueryBuilder();
+
+        QueryBuilderDirector director = new(queryBuilder);
+
+        var userProfileLink = director.CreateProfileLink();
+
+        Console.WriteLine(userProfileLink);
+    }
+}
